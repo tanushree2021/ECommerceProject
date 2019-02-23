56d9f3c313f8999995587c6b3bfab01fd8999184
Code WellnWill <code.wnw@gmail.com>
2019-02-23 19:31:06 +0530

    Loggin Aspect

--- src/com/trendingtwig/controller/ItemController.java
+++ src/com/trendingtwig/controller/ItemController.java
@@ -1,33 +1,41 @@
 package com.trendingtwig.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.trendingtwig.model.Item;
 import com.trendingtwig.service.ItemService;
 
 @Controller
+@RequestMapping(value = "/items")
 public class ItemController {
-	
+
 	private final ItemService itemService;
-	
+
 	@Autowired
 	public ItemController(ItemService itemService) {
 		this.itemService = itemService;
 	}
 
-	public Item getItem(Long id) {
+	@GetMapping(value = "/{id}")
+	public Item getItem(@PathVariable Long id) {
 		return itemService.getItem(id);
 	}
 
+	@GetMapping(value = "/")
 	public List<Item> getItems() {
 		return itemService.getItems();
 	}
 
-	public void saveItem(Item item) {
+	@PostMapping(value = "/")
+	public void saveItem(@RequestBody Item item) {
 		itemService.saveItem(item);
 	}
 
